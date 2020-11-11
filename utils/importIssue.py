cd7eb4ce375e6644e50a002623a2c84ebd28c2ae
Marco Zamboni <mazamboni@fbk.eu>
2020-11-11 19:04:02 +0100

    Update user stories names

--- utils/importIssue.py
+++ utils/importIssue.py
@@ -7,15 +7,16 @@ with open('issues.csv') as csv_file:
     last_name = ""
     last_description = ""
     for row in csv_reader:
+        if(row[0] != ""):
+            last_name = row[0]
+            last_description = row[1]
+        elif(row[0] == ""):
+            row[0] = last_name
+            row[1] = last_description
         if "Sviluppo UI" in row[2]:
-            if(row[0] != ""):
-                last_name = row[0]
-                last_description = row[1]
-            elif(row[0] == ""):
-                row[0] = last_name
-                row[1] = last_description
 
             os.system(f'gh issue create -p Frontend -t "UserStory: {row[0]} - {row[2]}" -b "{row[1]}"')
+            
             line_count += 1
     print(f'Processed {line_count} lines.')
 
